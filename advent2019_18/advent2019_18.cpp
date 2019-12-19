7d885746a770e295247fc311771aedcd335b3719
John Wiltshire <jw@chase.net.au>
2019-12-19 23:30:56 +1000

    Day 18: Rewrote A* again

--- advent2019_18/advent2019_18.cpp
+++ advent2019_18/advent2019_18.cpp
@@ -9,6 +9,7 @@
 #include <map>
 #include <list>
 #include <algorithm>
+#include <queue>
 
 #include "Point.h"
 
@@ -16,7 +17,7 @@ using namespace std;
 
 typedef vector<string> Map;
 typedef vector<bool> Keys;
-typedef list<char> Path;
+typedef vector<char> Path;
 typedef Point<int, 2> Point2;
 
 Point2 g_max;
@@ -44,9 +45,16 @@ bool IsDoor(char c)
 
 vector<bool> g_done;
 vector<bool> g_zero;
-int g_best = INT_MAX;
+
+struct KeyPath {
+    int dist;
+    set<char> doors;
+};
+
+typedef map<char, KeyPath> KeyPathMap;
+typedef map<pair<char, char>, KeyPath> Adjacency;
+Adjacency g_adjacency;
 int g_numkeys = 0;
-vector<vector<int> > g_dist(26, vector<int>(26));
 
 Infos GetItems(const Map& m, const Point2& pos, const Keys& keys)
 {
@@ -86,15 +94,14 @@ Infos GetItems(const Map& m, const Point2& pos, const Keys& keys)
     return result;
 }
 
-vector<int> GetDist(const Map& m, const Point2& pos)
+KeyPathMap GetDist(const Map& m, const Point2& pos)
 {
-    struct Data { Point2 where; int dist; };
+    struct Data { Point2 where; int dist; set<char> doors; };
     vector<Data> todo;
     todo.reserve(1000);
     todo.push_back(Data{ pos, 0 });
     g_done = g_zero;
-    vector<int> result;
-    result.resize(26);
+    KeyPathMap result;
 
     while (!todo.empty())
     {
@@ -108,11 +115,15 @@ vector<int> GetDist(const Map& m, const Point2& pos)
             if (newpos.InBounds(g_max)) {
                 if (!g_done[newpos[0] + g_max[0] * newpos[1]]) {
                     char c = m[newpos[1]][newpos[0]];
-                    if (c != '#') {
-                        todo.push_back(Data{ newpos, i.dist + 1 });
-                    }
+                    set<char> doors = i.doors;
                     if (IsKey(c)) {
-                        result[c - 'a'] = i.dist + 1;
+                        result[c] = KeyPath{ i.dist + 1, i.doors };
+                    }
+                    else if (IsDoor(c)) {
+                        doors.insert(tolower(c));
+                    }
+                    if (c != '#') {
+                        todo.push_back(Data{ newpos, i.dist + 1, doors });
                     }
                 }
             }
@@ -122,67 +133,148 @@ vector<int> GetDist(const Map& m, const Point2& pos)
     return result;
 }
 
-int GetMinDistIterative(const Map& m, const Point2& pos, Path& bestpath)
+int GetNoDoorDist(char start, const Keys& currkeys)
 {
-    int best = 0;
+    // Dijkstra - set up a queue
+    set<pair<int, char>> q;
+    map<char, int> distances;
+    set<char> keysleft;
 
-    struct ToDo {
-        Keys keys;
-        Point2 pos;
-        int dist;
-        int minleft;
-        // Only good for heaps, not stuff that needs strict less
-        bool operator< (const ToDo& other) const { return (dist + minleft*0.55) > (other.dist + other.minleft*0.55); }
-    };
-    vector<ToDo> todo;
-
-    Keys keys;
-    keys.resize(26);
-    int md = 0;
-    for (int i = 0; i < 25; ++i) md += g_dist[i][i + 1];
-    todo.push_back(ToDo{ keys, pos, 0, md });
-
-    make_heap(todo.begin(), todo.end());
-    while (!todo.empty()) {
-
-        ToDo item = todo.front();
-        pop_heap(todo.begin(), todo.end());
-        todo.pop_back();
-
-        int nk = 0;
-        for (const auto& x : item.keys) if (x) ++nk;
-        if (best < nk) {
-            cout << nk << ": " << todo.size() << endl;
-            best = nk;
+    // Figure out the set of keys we have to work with, and init
+    // their distances to a BIG number
+    for (char k = 'a'; k < 'a' + g_numkeys; ++k) {
+        if (!currkeys[k - 'a']) {
+            keysleft.insert(k);
+            distances[k] = INT_MAX;
         }
+    }
 
-        Infos infos = GetItems(m, item.pos, item.keys);
-        if (infos.empty()) {
-            return item.dist;
-        }
+    // Init queue with the starting pos at distance 0
+    int dist = 0;
+    q.insert(make_pair(0, start));
 
-        for (const auto& info : infos) {
-            if (IsKey(info.item)) {
-                ToDo newitem(item);
-                newitem.keys[info.item - 'a'] = true;
-                newitem.minleft = 0;
-                int j = 0;
-                for (int i = 1; i < 26; ++i) {
-                    if (!newitem.keys[i]) {
-                        newitem.minleft += g_dist[i][j];
-                        j = i;
-                    }
-                }
-                newitem.pos = info.pos;
-                newitem.dist += info.dist;
-                todo.push_back(newitem);
-                push_heap(todo.begin(), todo.end());
+    while (!q.empty()) {
+
+        // Grab shortest path off queue
+        auto p = *q.cbegin();
+        q.erase(q.cbegin());
+        dist = p.first;
+        char from = p.second;
+
+        // Fill all distances from the queue point out to other 
+        // points (because we can go everywhere)
+        for (char next : keysleft) {
+            auto index = make_pair(from, next);
+            const auto& kp = g_adjacency[index];
+            int newdist = kp.dist + dist;
+
+            // If we have a new best distance the remember it
+            // and push result on queue
+            if (distances[next] > newdist) {
+                q.erase(make_pair(distances[next], next));
+                distances[next] = newdist;
+                q.insert(make_pair(newdist, next));
             }
         }
     }
 
-    cout << "ERROR!!";
-    return -1;
+    return dist;
+}
+
+int FindShortestPath(Path& bestpath)
+{
+    // A* - set up a queue, with heuristics
+    struct Data {
+        int dist;
+        Path path;
+        Keys keys;
+        int min_total_dist;
+        bool operator<(const Data& other) const {
+            if (min_total_dist == other.min_total_dist) {
+                if (keys == other.keys) {
+                    return dist < other.dist;
+                }
+                return keys < other.keys;
+            }
+            return min_total_dist < other.min_total_dist;
+        }
+    };
+
+    // Don't bother initing the datamap - assume no entry is infinite
+    map<pair<char, Keys>, Data> datamap;
+    set<Data> q;
+    const Keys allkeys(g_numkeys, true);
+
+    // Init queue with the starting pos at distance 0 and no keys
+    Data item{ 0, Path{'@'}, Keys(g_numkeys) };
+    item.min_total_dist = GetNoDoorDist('@', item.keys);
+    q.insert(item);
+
+    // Some counters...
+    int count = 0;
+
+    while (!q.empty()) {
+
+        // Grab shortest path off queue
+        item = *q.cbegin();
+        q.erase(q.cbegin());
+
+        // Is it a winner?
+        if (item.keys == allkeys) {
+            break;
+        }
+
+        // Play with counter
+        ++count;
+        if (count % 100000 == 0) cout << count << "c: " << q.size() << item.min_total_dist << endl;
+
+        // Fill all distance from the queue point out to other points
+        // that we can get do (testing doors).
+        for (char next = 'a'; next < 'a' + g_numkeys; ++next) {
+
+            // If we already have the key, don't bother
+            if (item.keys[next - 'a']) continue;
+
+            // Get the adjacency item now we have (from, to)
+            char from = item.path.back();
+            auto index = make_pair(from, next);
+            const auto& kp = g_adjacency[index];
+
+            // Check the doors
+            bool all_doors = true;
+            for (const auto& d : kp.doors) {
+                if (!item.keys[d - 'a']) {
+                    all_doors = false;
+                    break;
+                }
+            }
+            if (!all_doors) continue;
+
+            // Compute new item
+            Data newitem(item);
+            newitem.dist += kp.dist;
+            newitem.keys[next - 'a'] = true;
+            newitem.path.push_back(next);
+            newitem.min_total_dist = newitem.dist + GetNoDoorDist(next, newitem.keys);
+
+            // If we have a new best distance, then remember it and
+            // push the result on queue
+            auto mapindex = make_pair(next, newitem.keys);
+            auto it = datamap.find(mapindex);
+            if (it == datamap.end()) {
+                q.insert(newitem);
+                datamap[mapindex] = newitem;
+            }
+            else if (it->second.min_total_dist > newitem.min_total_dist) {
+                q.erase(it->second);
+                q.insert(newitem);
+                datamap[mapindex] = newitem;
+            }
+        }
+    }
+
+    bestpath = item.path;
+    return item.dist;
 }
 
 int main()
@@ -203,19 +295,24 @@ int main()
 
     for (int y = 0; y < g_max[1]; ++y) {
         for (int x = 0; x < g_max[0]; ++x) {
-            if (m[y][x] == '@') {
+            char c = m[y][x];
+            if (c == '@') {
                 pos = Point2{ x, y };
-                m[y][x] = '.';
             }
-            if (IsKey(m[y][x])) {
-                g_dist[m[y][x] - 'a'] = GetDist(m, Point2{ x, y });
+            if (IsKey(c)) g_numkeys++;
+            if (IsKey(c) || c == '@') {
+                auto km = GetDist(m, Point2{ x, y });
+                for (const auto& it : km) {
+                    g_adjacency[make_pair(c, it.first)] = it.second;
+                }
             }
         }
     }
 
     Path path;
-    int dist = GetMinDistIterative(m, pos, path);
+    int dist = FindShortestPath(path);
     cout << "Min Dist = " << dist << endl;
     for (auto x : path) cout << x << ", ";
     cout << endl;
 }
+

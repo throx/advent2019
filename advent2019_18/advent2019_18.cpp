4c610f81ce20906ee2b6bf52c1f76852be969bcb
John Wiltshire <jw@chase.net.au>
2019-12-20 17:59:00 +1000

    Day 18 DONE FINALLY

--- advent2019_18/advent2019_18.cpp
+++ advent2019_18/advent2019_18.cpp
@@ -17,19 +17,10 @@ using namespace std;
 
 typedef vector<string> Map;
 typedef vector<bool> Keys;
-typedef vector<char> Path;
+typedef vector<string> Path;
 typedef Point<int, 2> Point2;
 
 Point2 g_max;
-
-struct Info {
-    char item;
-    int dist;
-    Point2 pos;
-    bool operator<(const Info& other) const { return dist > other.dist; }
-};
-
-typedef vector<Info> Infos;
 
 vector<Point2> DIRS = { Point2{-1,0}, Point2{1,0}, Point2{0,-1}, Point2{0,1} };
 
@@ -55,44 +46,7 @@ typedef map<char, KeyPath> KeyPathMap;
 typedef map<pair<char, char>, KeyPath> Adjacency;
 Adjacency g_adjacency;
 int g_numkeys = 0;
-
-Infos GetItems(const Map& m, const Point2& pos, const Keys& keys)
-{
-    // Flood fill
-    struct Data { Point2 where; int dist; };
-    vector<Data> todo;
-    todo.reserve(1000);
-    todo.push_back(Data{ pos, 0 });
-    g_done = g_zero;
-    Infos result;
-    while (!todo.empty())
-    {
-        auto i = todo.back();
-        char c = m[i.where[1]][i.where[0]];
-        todo.pop_back();
-        g_done[i.where[0] + g_max[0] * i.where[1]] = true;
-        if (IsKey(c) || IsDoor(c)) {
-            if (!keys[tolower(c) - 'a']) {
-                if (IsKey(c)) {
-                    result.push_back(Info{ c, i.dist, i.where });
-                }
-                continue;
-            }
-        }
-
-        for (auto dir : DIRS) {
-            Point2 newpos = i.where + dir;
-            if (newpos.InBounds(g_max)) {
-                if (!g_done[newpos[0] + g_max[0] * newpos[1]]) {
-                    if (m[newpos[1]][newpos[0]] != '#') {
-                        todo.push_back(Data{ newpos, i.dist + 1 });
-                    }
-                }
-            }
-        }
-    }
-    return result;
-}
+int g_numbots = 0;
 
 KeyPathMap GetDist(const Map& m, const Point2& pos)
 {
@@ -137,175 +91,34 @@ KeyPathMap GetDist(const Map& m, const Point2& pos)
     return result;
 }
 
-int GetNoDoorDist(char start, const Keys& currkeys)
-{
-    // Dijkstra - set up a queue
-    set<pair<int, char>> q;
-    map<char, int> distances;
-    set<char> keysleft;
-
-    // Figure out the set of keys we have to work with, and init
-    // their distances to a BIG number
-    for (char k = 'a'; k < 'a' + g_numkeys; ++k) {
-        if (!currkeys[k - 'a']) {
-            keysleft.insert(k);
-            distances[k] = INT_MAX;
-        }
-    }
-
-    // Init queue with the starting pos at distance 0
-    int bestdist = INT_MAX;
-    q.insert(make_pair(0, start));
-
-    while (!q.empty()) {
-
-        // Grab shortest path off queue
-        auto p = *q.cbegin();
-        q.erase(q.cbegin());
-        int dist = p.first;
-        char from = p.second;
-
-        // Fill all distances from the queue point out to other 
-        // points (because we can go everywhere)
-        for (char next : keysleft) {
-            auto index = make_pair(from, next);
-            const auto& kp = g_adjacency[index];
-            int newdist = kp.dist + dist;
-
-            // If we have a new best distance the remember it
-            // and push result on queue
-            if (distances[next] > newdist) {
-                q.erase(make_pair(distances[next], next));
-                distances[next] = newdist;
-                q.insert(make_pair(newdist, next));
-            }
-        }
-    }
-
-    return bestdist;
-}
-
-int FindShortestPathAStar(Path& bestpath)
-{
-    // A* - set up a queue, with heuristics
-    struct Data {
-        int dist;
-        Path path;
-        Keys keys;
-        int min_total_dist;
-        bool operator<(const Data& other) const {
-            if (min_total_dist == other.min_total_dist) {
-                if (keys == other.keys) {
-                    return dist < other.dist;
-                }
-                return keys < other.keys;
-            }
-            return min_total_dist < other.min_total_dist;
-        }
-    };
-
-    // Don't bother initing the datamap - assume no entry is infinite
-    map<pair<char, Keys>, Data> datamap;
-    set<Data> q;
-    const Keys allkeys(g_numkeys, true);
-
-    // Init queue with the starting pos at distance 0 and no keys
-    Data item{ 0, Path{'@'}, Keys(g_numkeys) };
-    item.min_total_dist = GetNoDoorDist('@', item.keys);
-    q.insert(item);
-
-    // Some counters...
-    int count = 0;
-
-    while (!q.empty()) {
-
-        // Grab shortest path off queue
-        item = *q.cbegin();
-        q.erase(q.cbegin());
-
-        // Is it a winner?
-        if (item.keys == allkeys) {
-            break;
-        }
-
-        // Play with counter
-        ++count;
-        if (count % 100000 == 0) cout << count << "c: " << q.size() << item.min_total_dist << endl;
-
-        // Fill all distance from the queue point out to other points
-        // that we can get do (testing doors).
-        for (char next = 'a'; next < 'a' + g_numkeys; ++next) {
-
-            // If we already have the key, don't bother
-            if (item.keys[next - 'a']) continue;
-
-            // Get the adjacency item now we have (from, to)
-            char from = item.path.back();
-            auto index = make_pair(from, next);
-            const auto& kp = g_adjacency[index];
-
-            // Check the doors
-            bool all_doors = true;
-            for (const auto& d : kp.doors) {
-                if (!item.keys[d - 'a']) {
-                    all_doors = false;
-                    break;
-                }
-            }
-            if (!all_doors) continue;
-
-            // Compute new item
-            Data newitem(item);
-            newitem.dist += kp.dist;
-            newitem.keys[next - 'a'] = true;
-            newitem.path.push_back(next);
-            newitem.min_total_dist = newitem.dist + GetNoDoorDist(next, newitem.keys);
-
-            // If we have a new best distance, then remember it and
-            // push the result on queue
-            auto mapindex = make_pair(next, newitem.keys);
-            auto it = datamap.find(mapindex);
-            if (it == datamap.end()) {
-                q.insert(newitem);
-                datamap[mapindex] = newitem;
-            }
-            else if (it->second.min_total_dist > newitem.min_total_dist) {
-                q.erase(it->second);
-                q.insert(newitem);
-                datamap[mapindex] = newitem;
-            }
-        }
-    }
-
-    bestpath = item.path;
-    return item.dist;
-}
-
 int FindShortestPathDijkstra(Path& bestpath)
 {
     // Dijkstra - set up a queue
     struct Data {
         int dist;
+        string pos;
         Path path;
         Keys keys;
         bool operator<(const Data& other) const {
             if (dist == other.dist) {
                 if (keys == other.keys) {
-                    return dist < other.dist;
+                    return pos < other.pos;
                 }
-                return keys < other.keys;
+                return keys > other.keys;
             }
             return dist < other.dist;
         }
     };
 
     // Don't bother initing the datamap - assume no entry is infinite
-    map<pair<char, Keys>, Data> datamap;
+    map<pair<string, Keys>, Data> datamap;
     set<Data> q;
     const Keys allkeys(g_numkeys, true);
 
     // Init queue with the starting pos at distance 0 and no keys
-    Data item{ 0, Path{'@'}, Keys(g_numkeys) };
+    string start;
+    for (int i = 0; i < g_numbots; ++i) start.push_back((char)('1' + i));
+    Data item{ 0, start, Path{start}, Keys(g_numkeys) };
     q.insert(item);
 
     // Some counters...
@@ -317,12 +130,21 @@ int FindShortestPathDijkstra(Path& bestpath)
         item = *q.cbegin();
         q.erase(q.cbegin());
 
+#ifdef TRACE
+        cout << "At " << item.path.back() << " at dist " << item.dist << " with keys [";
+        for (int x = 0; x < g_numkeys; ++x) if (item.keys[x]) cout << (char)('a' + x);
+        cout << "]" << endl;
+#endif
+
         // Is it a winner?
         if (item.keys == allkeys) {
+#ifdef TRACE
             cout << "Try " << item.dist << "? ";
             for (auto x : item.path) cout << x << ", ";
             cout << endl;
+#else
             break;
+#endif
         }
 
         // Play with counter
@@ -336,47 +158,64 @@ int FindShortestPathDijkstra(Path& bestpath)
             // If we already have the key, don't bother
             if (item.keys[next - 'a']) continue;
 
-            // Get the adjacency item now we have (from, to)
-            char from = item.path.back();
-            auto index = make_pair(from, next);
-            const auto& kp = g_adjacency[index];
+            // Check all bots
+            string botpos = item.pos;
+            for (int bot = 0; bot < g_numbots; ++bot) {
+                // Get the adjacency item now we have (from, to)
+                char from = botpos[bot];
+                auto index = make_pair(from, next);
 
-            // Check the doors
-            bool all_doors = true;
-            for (const auto& d : kp.doors) {
-                if (!item.keys[d - 'a']) {
-                    all_doors = false;
-                    break;
+                // Check if there is an adjacency
+                auto kp_it = g_adjacency.find(index);
+                if (kp_it == g_adjacency.end()) continue;
+                const auto& kp = kp_it->second;
+
+                // Check the doors
+                bool all_doors = true;
+                for (const auto& d : kp.doors) {
+                    if (!item.keys[d - 'a']) {
+                        all_doors = false;
+                        break;
+                    }
                 }
-            }
-            if (!all_doors) continue;
+                if (!all_doors) continue;
 
-            // Compute new item
-            Data newitem(item);
-            newitem.dist += kp.dist;
-            newitem.keys[next - 'a'] = true;
-            newitem.path.push_back(next);
+                // Compute new item
+                Data newitem(item);
+                newitem.dist += kp.dist;
+                newitem.keys[next - 'a'] = true;
+                newitem.pos[bot] = next;
+                newitem.path.push_back(newitem.pos);
 
-            // If we have a new best distance, then remember it and
-            // push the result on queue
-            auto mapindex = make_pair(next, newitem.keys);
-            auto it = datamap.find(mapindex);
-            if (it == datamap.end()) {
-                q.insert(newitem);
-                datamap[mapindex] = newitem;
+                // If we have a new best distance, then remember it and
+                // push the result on queue
+                auto mapindex = make_pair(newitem.pos, newitem.keys);
+                auto it = datamap.find(mapindex);
+                if (it == datamap.end()) {
+                    q.insert(newitem);
+                    datamap[mapindex] = newitem;
 
-                //cout << "Add " << next << " at dist " << newitem.dist << " with keys [";
-                //for (int x = 0; x < g_numkeys; ++x) if (newitem.keys[x]) cout << (char)('a'+x);
-                //cout << "]" << endl;
-            }
-            else if (it->second.dist > newitem.dist) {
-                q.erase(it->second);
-                q.insert(newitem);
-                datamap[mapindex] = newitem;
-
-                //cout << "Overwrite " << next << " at dist " << newitem.dist << " with keys [";
-                //for (int x = 0; x < g_numkeys; ++x) if (newitem.keys[x]) cout << (char)('a' + x);
-                //cout << "]" << endl;
+#ifdef TRACE
+                    cout << "Add " << newitem.pos << " at dist " << newitem.dist << " with keys [";
+                    for (int x = 0; x < g_numkeys; ++x) if (newitem.keys[x]) cout << (char)('a'+x);
+                    cout << "]" << endl;
+#endif
+                }
+                else if (it->second.dist > newitem.dist) {
+#ifdef TRACE
+                    cout << "Overwrite " << it->second.pos << " at dist " << it->second.dist << " with keys [";
+                    for (int x = 0; x < g_numkeys; ++x) if (it->second.keys[x]) cout << (char)('a' + x);
+                    cout << "]" << endl;
+#endif
+                    q.erase(it->second);
+                    q.insert(newitem);
+                    datamap[mapindex] = newitem;
+#ifdef TRACE
+                    cout << "     with " << newitem.pos << " at dist " << newitem.dist << " with keys [";
+                    for (int x = 0; x < g_numkeys; ++x) if (newitem.keys[x]) cout << (char)('a' + x);
+                    cout << "]" << endl;
+#endif
+                }
             }
         }
     }
@@ -389,7 +228,7 @@ int main()
 {
     Map m;
 
-    ifstream f("Data.txt");
+    ifstream f("Data2.txt");
     while (!f.eof()) {
         string s;
         getline(f, s);
@@ -399,16 +238,15 @@ int main()
     g_done.resize(g_max[0] * g_max[1]);
     g_zero.resize(g_max[0] * g_max[1]);
 
-    Point2 pos;
-
     for (int y = 0; y < g_max[1]; ++y) {
         for (int x = 0; x < g_max[0]; ++x) {
             char c = m[y][x];
-            if (c == '@') {
-                pos = Point2{ x, y };
-            }
             if (IsKey(c)) g_numkeys++;
             if (IsKey(c) || c == '@') {
+                if (c == '@') {
+                    ++g_numbots;
+                    c = '0' + g_numbots;
+                }
                 auto km = GetDist(m, Point2{ x, y });
                 for (const auto& it : km) {
                     g_adjacency[make_pair(c, it.first)] = it.second;
